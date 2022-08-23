24510d41cfbc52cec22ae01c391840bc6598fc55
Manan Mukim <mukimmanan@gmail.com>
2022-08-23 14:39:18 +0530

    Connected User Model

---
+++ models/user.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+
+const User = new Schema({
+    username: {
+        type: String,
+        required: trusted,
+    },
+    password: {
+        type: Schema.Types.String,
+        required: true
+    },
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    }
+}, {timestamps: true})
+
+module.exports = mongoose.model("User", User)

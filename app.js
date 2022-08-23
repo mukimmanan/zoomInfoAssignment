b1239288b1646924fccee64d41416f129d568595
Manan Mukim <mukimmanan@gmail.com>
2022-08-23 14:21:37 +0530

    Created a test endpoint

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ const express = require("express")
 app = express()
 PORT = 8080
 
+// Adding a test route
+app.get("/test", (req, res, next) => {
+    return res.json({
+        message: "Your server is working fine"
+    })
+})
+
 app.listen(PORT, (error) => {
     if (!error) {
         console.log("Server is successfully created and is Running on Port = " + PORT)

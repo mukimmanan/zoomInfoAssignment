2baea2bb2b27f5f05f4f2bfb8c3082e3723a44b0
Manan Mukim <mukimmanan@gmail.com>
2022-08-23 15:15:27 +0530

    Added Signup & Signin methods

--- controllers/auth.js
+++ controllers/auth.js
@@ -25,3 +25,25 @@ exports.signup = (req, res, next) => {
         message: "Error Occurred"
     })
 }
+
+exports.signin = (req, res, next) => {
+    if (req.body.username && req.body.password) {
+        User.findOne({
+            username: req.body.username,
+            password: req.body.password
+        }).then(result => {
+            res.status(200).json({
+                message: "User Found",
+                userid: result._id
+            })
+        }).catch(error => {
+            res.status(400).json({
+                message: "Error Occurred"
+            })  
+        })
+    }
+
+    return res.status(400).json({
+        message: "Error Occurred"
+    })
+}

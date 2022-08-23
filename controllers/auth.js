8dc473fa59638560acfc5b6b47f984e519418705
Manan Mukim <mukimmanan@gmail.com>
2022-08-23 19:18:07 +0530

    Added Password hashing

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,10 +1,15 @@
 const User = require("../models/user")
+const crypto = require("crypto")
 
 exports.signup = (req, res, next) => {
     if (req.body.username && req.body.password && req.body.firstName && req.body.lastName) {
+        salt = "frfkjsdncfjksdncjksnd"; 
+        hash = crypto.pbkdf2Sync(req.body.password, salt,  
+            1000, 64, `sha512`).toString(`hex`); 
+        // password``
         newUser = new User({
             username: req.body.username,
-            password: req.body.password,
+            password: hash,
             lastName: req.body.lastName,
             firstName: req.body.firstName
         })
@@ -27,23 +32,34 @@ exports.signup = (req, res, next) => {
 }
 
 exports.signin = (req, res, next) => {
+    console.log(req.body)
     if (req.body.username && req.body.password) {
-        User.findOne({
+        salt = "frfkjsdncfjksdncjksnd"; 
+        hash = crypto.pbkdf2Sync(req.body.password, salt,  
+            1000, 64, `sha512`).toString(`hex`); 
+
+        return User.findOne({
             username: req.body.username,
-            password: req.body.password
+            password: hash
         }).then(result => {
+            if (!result) {
+                res.status(400).json({
+                    message: "Error Occurred 1"
+                }) 
+            }
             res.status(200).json({
                 message: "User Found",
-                userid: result._id
+                id: result._id
             })
         }).catch(error => {
+            console.log(error)
             res.status(400).json({
-                message: "Error Occurred"
+                message: "Error Occurred 1"
             })  
         })
     }
 
     return res.status(400).json({
-        message: "Error Occurred"
+        message: "Error Occurred 2"
     })
 }

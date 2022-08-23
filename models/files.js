c4c96eceb7e91561ef9c2c238f43078c583ef329
Manan Mukim <mukimmanan@gmail.com>
2022-08-23 16:10:30 +0530

    Added File Model

---
+++ models/files.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const File = new Schema({
+    content: {
+        type: String,
+        required: true
+    },
+
+    userid: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+        index: true
+    },
+
+    folderid: {
+        type: Schema.Types.ObjectId,
+        ref: 'Folder',
+        required: false
+    }
+
+}, {timestamps: true})
+
+module.exports = Schema.model("File", File)

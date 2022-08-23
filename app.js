1bc97409183c5eb82621841d1ed5dd2ce74d34c9
Manan Mukim <mukimmanan@gmail.com>
2022-08-23 14:35:57 +0530

    Connected MongoDB database

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require("express")
 const bodyParse = require("body-parser")
+const moongoose = require("mongoose")
+const { default: mongoose } = require("mongoose")
 
 app = express()
 PORT = 8080
 
 // Adding JSON Body Parser
 app.use(bodyParse.json())
-
 
 // Adding a test route
 app.get("/test", (req, res, next) => {
@@ -15,10 +16,12 @@ app.get("/test", (req, res, next) => {
     })
 })
 
-app.listen(PORT, (error) => {
-    if (!error) {
-        console.log("Server is successfully created and is Running on Port = " + PORT)
-    } else {
-        console.log("Error Occurred, server can't start")
-    }
-})
+mongoose.connect("mongodb://localhost:27017/assignmentDB").then(_ => {
+    app.listen(PORT, (error) => {
+        if (!error) {
+            console.log("Server is successfully created and is Running on Port = " + PORT)
+        } else {
+            console.log("Error Occurred, server can't start")
+        }
+    })
+}).catch(error => console.log(error))
